20b5b78e9e6e3e9300ea6627af861bf0fe54ea17
Benediktas Rukas <39025147+benycode@users.noreply.github.com>
2022-05-09 16:23:47 +0300

    Add more stick logging

--- gelf.go
+++ gelf.go
@@ -83,7 +83,7 @@ type GelfMessage struct {
 }
 
 func (m GelfMessage) getExtraFields() (json.RawMessage, error) {
-        log.Println("m:", m)
+        log.Println("m:", m.Container)
 	extra := map[string]interface{}{
 		"_container_id":   m.Container.ID,
 		"_container_name": m.Container.Name[1:], // might be better to use strings.TrimLeft() to remove the first /
